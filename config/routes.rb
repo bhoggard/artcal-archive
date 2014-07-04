948d8ddd7143839a6a727c962cc90c1f96470914
Barry Hoggard <barry@tristanmedia.com>
2014-07-04 16:59:17 -0400

    added contact/about/legal

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
 
   get 'event/view/:neighborhood_id/:id', to: redirect('/exhibits/%{id}')
 
+  get 'contact' => 'home#contact'
+  get 'about' => 'home#about'
+  get 'legal' => 'home#legal'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
